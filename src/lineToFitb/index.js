29deb7ba8cbb0218b68a33bd376a6635760fffc8
mattia richetto <mattia.richetto@gmail.com>
2019-12-15 18:29:22 +0200

    add lineToFitb

---
+++ src/lineToFitb/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const { join } = require('path');
+const lineToFitb = require('./lineToFitb');
+
+const { FILE_NAME } = process.env;
+
+const lines = fs
+  .readFileSync(join(__dirname, `${FILE_NAME}.txt`), 'utf-8')
+  .split('\n')
+  .filter(Boolean);
+
+const fitbs = lines.map(line =>
+  lineToFitb({ id: Date.now().toString(), line })
+);
+
+fs.writeFileSync(
+  join(__dirname, `${FILE_NAME}.json`),
+  JSON.stringify(fitbs, 0, 2)
+);
